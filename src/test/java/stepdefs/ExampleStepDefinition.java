983c35c0f04f4b9c1945b9f13fd1c2a7642a205d
Simran Vasir <simran.vasir@sap.com>
2019-08-13 18:18:44 -0700

    First commit.
    Configured pom.xml. Added dependencies. Setup example feature, step definition and test runner and built the initial project structure.
    
    Signed-off-by: Simran Vasir <simran.vasir@sap.com>

---
+++ src/test/java/stepdefs/ExampleStepDefinition.java
@@ -0,0 +1,40 @@
+package stepdefs;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.testng.Assert;
+
+public class ExampleStepDefinition {
+    @Given("^I am on the something page on URL$")
+    public void i_am_on_the_something_page_on_URL() throws Throwable {
+        Assert.assertEquals(8,8);
+    }
+
+    @When("I fill in {word} blah {word} with blah")
+    public void i_fill_in_with(String arg1, String arg2) throws Throwable {
+        Assert.assertEquals(9, 8, "I fill in");
+
+        throw new PendingException();
+    }
+
+    @When("^I click on the \"([^\"]*)\" button$")
+    public void i_click_on_the_button(String arg1) throws Throwable {
+        Assert.assertEquals("blah", "blah");
+        throw new PendingException();
+    }
+
+    @Then("^I should see \"([^\"]*)\" message$")
+    public void i_should_see_message(String arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^I should see the \"([^\"]*)\" button$")
+    public void i_should_see_the_button(String arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+}
---
+++ src/test/resources/features/example.feature
@@ -0,0 +1,19 @@
+Feature: Example feature
+  As an automation architect
+  I want to develop automation framework
+  So that we can write UI tests in the framework
+
+ Scenario: example scenario
+ Given I am on the something page on URL
+ When I fill in blah with blah
+
+
+# Scenario Outline: title
+#  Given .... <placeholder 1> .....
+#  When .... <placeholder 2> ...
+#  Then ... <placeholder 3> ...
+#
+#  Examples:
+#    | placeholder 1 | placeholder 2 | placeholder 3 |
+#    |  value        |   value       |  value        |
+#    |  value        |   value       |  value        |
